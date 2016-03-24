62cc31d300cbafa8fbe43d4ad07bbda17a46b855
Nisha Kanani <nkanani@ac.com>
2016-03-24 09:22:31 -0400

    Jasmine Testing example

---
+++ WebSite1/AngularExample/Controller/app.js
@@ -0,0 +1,15 @@
+﻿angular.module('app', [])
+.controller('PasswordController', function PasswordController($scope) {
+        $scope.password = "hello";
+        $scope.grade = function () {
+            "use strict";
+        var size = $scope.password.length;
+        if (size > 8) {
+            $scope.strength = 'strong';
+        } else if (size > 3) {
+            $scope.strength = 'medium';
+        } else {
+            $scope.strength = 'weak';
+        }
+    };
+});
